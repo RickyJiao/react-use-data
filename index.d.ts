bfaa91dfbff0b5300f3df4e04cc5f6c8cfeeba5e
RickyJiao <jiaozhi_tian@sina.com>
2019-04-23 17:56:34 +0800

    update typing definition

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,58 @@
 export as namespace ReactUseData;
 
-export function useDetail(option: useDetailParams): string;
+export function useDetail(option: useDetailParams): useDetailHook;
 
-export function useList(option: useListParams): number;
+export function useList(option: useListParams): useListHook;
+
+declare type useDetailHook = (name: string) => UseDetailHook;
+
+declare type useListHook = (name: string) => UseListHook;
+
+export interface UseDetailHook {
+  detail: any;
+  isFetching: boolean;
+}
+
+export interface UseListHook {
+  detail: any;
+  isFetching: boolean;
+  data: Array<any>;
+  meta: ListMeta;
+  hasMore: boolean;
+  loadMore: () => void;
+}
+
 
 export interface DetailResponse {
-  data: any
+  data: any;
 }
 
 export interface ListMeta {
-  page: number,
-  pageSize: number,
-  totalPage: number
+  page: number;
+  pageSize: number;
+  totalPage: number;
 }
 
 export interface ListResponse {
-  meta: ListMeta,
+  meta: ListMeta;
   data: Array<any>
 }
 
 export interface ListRequest {
-  page: number,
-  pageSize: number,
-  context: string | null
+  page: number;
+  pageSize: number;
+  context: string | null;
 }
 
 export interface useDetailParams {
-  fetchData: (context: string) => Promise<DetailResponse>,
-  initalState: any,
-  defaultData: any
+  fetchData: (context: string) => Promise<DetailResponse>;
+  initalState: any;
+  defaultData: any;
 }
 
 export interface useListParams {
-  fetchData: (option: ListRequest) => Promise<ListResponse>,
-  initalState: any,
-  defaultData: any,
-  pageSize: number
+  fetchData: (option: ListRequest) => Promise<ListResponse>;
+  initalState: any;
+  defaultData: any;
+  pageSize: number;
 }
